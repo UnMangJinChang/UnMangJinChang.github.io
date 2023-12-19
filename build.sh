7e29c8982a4d0a581cc2840f6e8da72f4bcf252d
UnMangJinChang <lovektw3855@naver.com>
2023-12-19 16:24:19 +0900

    Added hash compare to build.sh for efficiency.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-clang++ -g -o main-content-creator -std=c++20 main-content-creator.cpp
+cur_hash=`echo -n main-content-creator.cpp | sha256sum | cut -d' ' -f1`
+if [ ! -f ./main-content-creator-hash ] || [ `cat main-content-creator-hash` != ${cur_hash} ]; then
+    clang++ -g -o main-content-creator -std=c++20 main-content-creator.cpp
+fi
+echo ${cur_hash} > main-content-creator-hash
 ./main-content-creator
-clang++ -g -o page-creator -std=c++20 page-creator.cpp
+cur_hash=`echo -n page-creator.cpp | sha256sum | cut -d' ' -f1`
+if [ ! -f ./page-creator-hash ] || [ `cat page-creator-hash` != ${cur_hash} ]; then
+    clang++ -g -o page-creator -std=c++20 page-creator.cpp
+fi
+echo ${cur_hash} > page-creator-hash
 ./page-creator
